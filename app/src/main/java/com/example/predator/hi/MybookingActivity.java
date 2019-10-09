fe7aac9c97b10ca273629da8916ef38015804c95
Wafiy <Wafiyhajibrahim@gmail.com>
2019-10-09 23:33:34 +0800

    Complete to display kelas mengaji bookings

--- app/src/main/java/com/example/predator/hi/MybookingActivity.java
+++ app/src/main/java/com/example/predator/hi/MybookingActivity.java
@@ -2,11 +2,19 @@ package com.example.predator.hi;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -24,9 +32,12 @@ public class MybookingActivity extends AppCompatActivity {
     private FirebaseAuth firebaseAuth;
     private DatabaseReference FirebaseBookingMengaji;
     private DatabaseReference queryMengaji;
-    ListView bookingMengajiList;
-    List<HuffazBookingClass> listMengaji;
+    //ListView bookingMengajiList;
+    //List<HuffazBookingClass> listMengaji;
+    RecyclerView bookingMengajiList;
+    FirebaseRecyclerAdapter adapter;
 
+    //@SuppressLint("WrongConstant")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,12 +52,39 @@ public class MybookingActivity extends AppCompatActivity {
 
         queryMengaji = FirebaseBookingMengaji.child(accountUser);
 
-        bookingMengajiList = (ListView) findViewById(R.id.bookingMengajiList);
+        bookingMengajiList = (RecyclerView) findViewById(R.id.bookingMengajiList);
 
-        listMengaji  = new ArrayList<>();
+        FirebaseRecyclerOptions<HuffazBookingClass> options = new FirebaseRecyclerOptions.Builder<HuffazBookingClass>().setQuery(queryMengaji, HuffazBookingClass.class).build();
+
+        adapter = new FirebaseRecyclerAdapter<HuffazBookingClass, HuffazBookingClassHolder>(options) {
+
+            @NonNull
+            @Override
+            public HuffazBookingClassHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.mengaji_layout, parent, false);
+                return new HuffazBookingClassHolder(view);
+            }
+
+            @Override
+            protected void onBindViewHolder(@NonNull HuffazBookingClassHolder holder, int position, @NonNull HuffazBookingClass model) {
+                holder.spinnerPackage.setText(model.getChoosePackage());
+                holder.spinnerTeacher.setText(model.getChooseTeacher());
+                holder.spinnerDay.setText(model.getChooseDay());
+                holder.preferredTime.setText(model.getTimePreference());
+                holder.HomeAddress.setText(model.getClientAddress());
+                holder.bookingStatus.setText(model.getBookingStatus());
+            }
+        };
+
+        bookingMengajiList.setAdapter(adapter);
+        bookingMengajiList.setHasFixedSize(true);
+        bookingMengajiList.setLayoutManager(new LinearLayoutManager(this));
+
+        //listMengaji  = new ArrayList<>();
 
         //Retrieve data mengaji
-        queryMengaji.addValueEventListener(new ValueEventListener() {
+        /*queryMengaji.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
 
@@ -69,14 +107,20 @@ public class MybookingActivity extends AppCompatActivity {
             public void onCancelled(@NonNull DatabaseError databaseError) {
 
             }
-        });
+        });*/
 
     }
 
     @Override
     protected void onStart() {
         super.onStart();
+        adapter.startListening();
 
     }
 
+    @Override
+    protected void onStop() {
+        super.onStop();
+        adapter.startListening();
+    }
 }
